340d9a62be94449fdaf648f4894c081886aaa5c4
Yasin_85_123 <yasin.85.123@gmail.com>
2026-08-04 23:54:52 +0330

    a simple starter program to start with using RtAudio and see my input and output devices and their infos, and print them using a for loop and all

--- note detection.cpp
+++ note detection.cpp
@@ -5,15 +5,32 @@
 
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using json = nlohmann::json;
 
 int main() 
 {
-    RtAudio rt_audio;
+    RtAudio audio;
 
+    unsigned int device_count = audio.getDeviceCount();
 
+    std::vector<RtAudio::DeviceInfo> info;
+    info.reserve(device_count);
 
+    for (int i = 0; i < device_count; i++)
+    {
+        info.push_back(audio.getDeviceInfo(i));
+    }
+
+    for (const auto& v : info)
+    {
+        std::string input = v.isDefaultInput ? "default input" : "not default input";
+        std::string output = v.isDefaultOutput ? "default output" : "not default output";
+        
+        std::cout << v.name << " Duplex channels : " << v.duplexChannels << ", " << input << ", " << output << std::endl;
+        std::cout << "input channels : " << v.inputChannels << ", output channels : " << v.outputChannels << std::endl << std::endl;
+    }
 
     return 0;
 }
