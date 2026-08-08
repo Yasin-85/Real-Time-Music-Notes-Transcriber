a2d81ff49407462a35c85527c4e6397f5e3ad70f
Yasin_85_123 <yasin.85.123@gmail.com>
2026-08-09 00:18:46 +0330

    added a simple UI and settings for the program to have
    decided to go linear program instead of oop for better performance
    
    next to do : add in the pitch detection, storing, printing and more in the record_call_back function

--- note detection.cpp
+++ note detection.cpp
@@ -9,7 +9,13 @@
 
 using json = nlohmann::json;
 
+struct Note
+{
+	int beat, subdivision, note, duration;
+};
+
 static int record_callback(void* output_buffer, void* input_buffer, unsigned int n_frames, double stream_time, RtAudioStreamStatus status, void* user_data);
+void print_line();
 
 int main()
 {
@@ -23,6 +29,11 @@ int main()
 	std::vector<double> audio_buffer;
 	unsigned int buffer_size = 256;
 
+	int choice;
+
+	double BPM{ 0 };
+	int time_sig_num{ 0 }, time_sig_den{ 0 }, subdivision{ 0 };
+
 	try
 	{
 		audio.openStream(nullptr, &params, RTAUDIO_FLOAT64, 44100, &buffer_size, &record_callback, &audio_buffer);
@@ -33,40 +44,92 @@ int main()
 		return 1;
 	}
 
-	RtAudio::DeviceInfo info = audio.getDeviceInfo(params.deviceId);
-	std::cout << "Using microphone: " << info.name << std::endl;
-
-	std::cout << "press enter to start recording..." << "\n";
-	std::cin.get();
-
-	try
+	while (true)
 	{
-		audio.startStream();
-	}
-	catch (RtAudioError& e)
-	{
-		std::cerr << "error starting stream : " << e.getMessage() << '\n';
-	}
+		print_line();
+		std::cout << "REAL TIME NOTE TRANSCRIBER\n";
+		print_line();
 
-	std::cout << "recording, press enter to stop..." << '\n';
-	std::cin.get();
+		std::cout << "1. start recording \n"
+			"2. settings \n"
+			"3. exit\n"
+			"your choice : ";
 
-	try 
-	{
-		audio.stopStream();
-		audio.closeStream();
-	}
-	catch (RtAudioError& e) 
-	{
-		std::cerr << "error stopping stream: " << e.getMessage() << std::endl;
-		return 1;
-	}
+		std::cin >> choice;
 
-	std::cout << "Recorded " << audio_buffer.size() << " samples." << std::endl;
+		switch (choice)
+		{
+		case 1:
+			break;
+		
+		case 2:
+		{
+			RtAudio::DeviceInfo info = audio.getDeviceInfo(params.deviceId);
+
+			std::cout << "current settings\n";
+			std::cout << "defualt microphone : " << info.name << '\n';
+			std::cout << "(grid settings) \n"
+				"BPM : " << BPM << ", time signiture : " << time_sig_num << "/" << time_sig_den << ", dubdivision : " << subdivision << ",(0 is the unset defualt value)\n";
+
+			char choice{ 'A' };
+			while (true)
+			{
+				std::cout << "change grid settings ? (Y/N) ";
+				std::cin >> choice;
+
+				if (choice == 'y' || choice == 'Y')
+				{
+					std::cout << "enter BPM: ";
+					std::cin >> BPM;
+
+					std::cout << "enter time signature (e.g., 4 4, 6 8, 16 8): ";
+					std::cin >> time_sig_num >> time_sig_den;
+
+					do
+					{
+						std::cout << "enter subdivision (e.g., 4, 8, 16, 32), (cannot be smaller than the time signiture's den and must be a multiple of time signiture's den): ";
+						std::cin >> subdivision;
+					} while (subdivision < time_sig_den || subdivision % time_sig_den != 0);
+				}
+				else if (choice == 'n' || choice == 'N')
+				{
+					std::cout << "going back\n";
+					break;
+				}
+				else
+				{
+					std::cout << "invalid choice entered\n";
+				}
+			}
+			break;
+		}
+
+		case 3:
+
+			std::cout << "thanks for using this program\n";
+			
+			try
+			{
+				if (audio.isStreamOpen()) 
+				{
+					audio.closeStream();
+				}
+			}
+			catch (RtAudioError& e)
+			{
+				std::cerr << "error closing stream: " << e.getMessage() << std::endl;
+				return 1;
+			}
+			return 0;
+
+		default:
+			std::cout << "invalid input entered\n";
+			break;
+		}
+	}
 
 	return 0;
 }
-
 
 static int record_callback(void* output_buffer, void* input_buffer, unsigned int n_frames, double stream_time, RtAudioStreamStatus status, void* user_data)
 {
@@ -80,4 +143,9 @@ static int record_callback(void* output_buffer, void* input_buffer, unsigned int
 	}
 
 	return 0;
+}
+
+void print_line()
+{
+	std::cout << "##########################################" << '\n';
 }
